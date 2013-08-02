a7d37412eb135b0cf0f67128052fd65a4fe97fe9
Karl Weber <karloscarweber@gmail.com>
2013-08-01 20:00:11 -0600

    initial commit

---
+++ index.php
@@ -0,0 +1,48 @@
+<!doctype html>
+<html>
+<head>
+	<title>Niko Is 1</title>
+	<link href='http://fonts.googleapis.com/css?family=Lato:300,400,700,300italic' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" type="text/css" href="main.css">
+	<link rel="stylesheet" type="text/css" href="grater.css">
+</head>
+<body>
+<!--
+	<div class="megaContainer">
+		<img src="prologueLarge.jpg">
+	</div>
+-->
+	<div class="grater-container">
+		<div class="grater">
+			<h1>Prologue</h1>
+		</div>
+		<div class="grater">
+			<div>
+				<img src="book.png">
+				<h4>For Readers:</h4>
+				<p>The Best Blogs<br>All in One Place.</p>
+			</div>
+			<div>
+				<img src="pencil.png">
+				<h4>For Writers:</h4>
+				<p>The Best Way to Share<br>&amp; Sell your Prose.</p>
+			</div>
+		</div>
+		<div class="grater">
+			<p>Launching this Fall...<!--<br>Receive an Email when we launch.--></p>
+		</div>
+		<!--
+		<div class="email">
+			<input class="email" type="text" name="email" placeholder="someone@someplace.com">
+			<a href="" class="email button"></a>
+		</div>
+		-->
+
+	</div>
+	<div class="footer">
+		<div>
+			<p>All things &copy; Super Powered Pixels.</p>
+		</div>
+	</div>	
+</body>
+</html>
